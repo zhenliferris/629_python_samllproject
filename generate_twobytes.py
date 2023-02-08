e93b5bb582389ff382280170c863abe1b831c2b4
zhenliferris <liz6@ferris.edu>
2023-02-07 23:01:44 -0500

    add reqirements for two py files

---
+++ generate_twobytes.py
@@ -0,0 +1,5 @@
+"""Build a program to generates 2 Bytes for an AV:
+a. Byte1: angle of steering values from 0 to 359
+  (how many bits do you need ?)
+b. Byte 2 : the first bit is allocated to the previous task,
+   7 other bits are dedicated to represent speed in miles. (0-127)"""
---
+++ read_numbers.py
@@ -0,0 +1,4 @@
+"""Build  a program to:
+a. read an integer number from the user.
+b. repeat the following until the number is 0.
+c. shift it to the right and print it."""
