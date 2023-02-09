cb72e47873c4879547af99643feb48a5e0b05643
zhenliferris <liz6@ferris.edu>
2023-02-09 18:37:32 -0500

    rename module and complete code

---
+++ int_right_shift.py
@@ -0,0 +1,47 @@
+"""Build  a program to:
+a. read an integer number from the user. isinstance(x, int)
+    123/10 = 12
+    12/10 = 1
+    1/10 = 0  
+b. repeat the following until the number is 0.
+c. shift it to the right and print it."""
+
+
+def int_right_shift():
+    # a loop keeps prompt user to input an integer number
+    while True:
+        user_enter = input("Please enter a integer number:")
+
+        # in case user input positive or negative sign
+        if user_enter[0] in ["+", "-"]:
+            if user_enter[1:].isdigit():
+                number = int(user_enter)
+                break  # break from the loop if user enter an integer
+            else:
+                print(
+                    "{} is not an integer. The right shift can't be processed.".format(user_enter))
+        else:
+            if user_enter.isdigit():
+                number = int(user_enter)
+                break  # break from the loop if user enter an integer
+            else:
+                print(
+                    "{} is not an integer. The right shift can't be processed.".format(user_enter))
+
+    if number == 0:
+        print("you input is: {} the right shift is not processed.".format(number))
+    elif number > 0:
+        print("you input is: {} the right shift is processing:".format(number))
+        while number != 0:
+            number //= 10  # right shift processing
+            print(number)
+    else:
+        print("you input is: {} the right shift is processing:".format(number))
+        number *= -1  # get absolute value from the negative integer
+        while number != 0:
+            number //= 10
+            print(-1*number)  # reflecting the negative value
+
+
+if __name__ == '__main__':
+    int_right_shift()
--- read_numbers.py
+++
@@ -1,4 +0,0 @@
-"""Build  a program to:
-a. read an integer number from the user.
-b. repeat the following until the number is 0.
-c. shift it to the right and print it."""
